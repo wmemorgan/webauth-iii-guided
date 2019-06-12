a5c574189679b27d7c20757679f5737d0b1fde9e
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-06-12 13:49:32 +0800

    Switch to token verification

--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -1,23 +1,22 @@
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken')
 
+const secrets = require('../config/secrets')
 const Users = require('../users/users-model.js');
 
 module.exports = (req, res, next) => {
-  const { username, password } = req.headers;
+  const token = req.headers.authorization;
 
-  if (username && password) {
-    Users.findBy({ username })
-      .first()
-      .then(user => {
-        if (user && bcrypt.compareSync(password, user.password)) {
-          next();
-        } else {
-          res.status(401).json({ message: 'Invalid Credentials' });
-        }
-      })
-      .catch(error => {
-        res.status(500).json({ message: 'Ran into an unexpected error' });
-      });
+  if (token) {
+   jwt.verify(token, secrets.jwt, (err, payload) => {
+     if (err) {
+       res.status(403).json({ message: `You are not authorized` })
+     } else {
+       req.userId = payload.userId
+       next()
+     }
+   })
+
   } else {
     res.status(400).json({ message: 'No credentials provided' });
   }
